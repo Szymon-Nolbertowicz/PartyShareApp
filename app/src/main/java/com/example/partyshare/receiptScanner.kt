efe90e677936e354cd10ce0931c4bfbb1f47c6d6
Szymon-Nolbertowicz <89022660+Szymon-Nolbertowicz@users.noreply.github.com>
2021-09-17 15:41:36 +0200

    crop image

--- app/src/main/java/com/example/partyshare/receiptScanner.kt
+++ app/src/main/java/com/example/partyshare/receiptScanner.kt
@@ -1,13 +1,11 @@
 package com.example.partyshare
 
-import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
@@ -16,34 +14,36 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.canhub.cropper.CropImage
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import com.yalantis.ucrop.UCrop
-import java.io.File
 
 
 class receiptScanner : AppCompatActivity() {
 
 
     private lateinit var recognizer:TextRecognizer
-    private lateinit var uri: Uri
-    private lateinit var destUri: Uri
-    private lateinit var resultUri: Uri
-    private lateinit var resUri: Uri
     private lateinit var ivCroppedImage: ImageView
     private lateinit var tvResult: TextView
+    private lateinit var cropActivityResultLauncher: ActivityResultLauncher<Any?>
 
     private val TAG = "ML_Kit_text_recognition"
     private val testImage = R.drawable.testimage1
     private var selectedImage:Bitmap? = null
 
+    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri?>() {
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .getIntent(this@receiptScanner)
+        }
 
-
-
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uriContent
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,9 +57,17 @@ class receiptScanner : AppCompatActivity() {
         val btnBack = findViewById<ImageView>(R.id.onBackToMenu)
         val btnExtract = findViewById<Button>(R.id.btnExtractText)
         tvResult = findViewById<EditText>(R.id.tvResult)
+        cropActivityResultLauncher = registerForActivityResult(cropActivityResultContract) {
+            it?.let {
+                uri ->
+                ivCroppedImage.setImageURI(uri)
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+                selectedImage = bitmap
+            }
+        }
 
         btnChooseImage.setOnClickListener{
-            openDialog()
+            cropActivityResultLauncher.launch(null)
         }
 
         btnExtract.setOnClickListener {
@@ -74,8 +82,6 @@ class receiptScanner : AppCompatActivity() {
 
         ivCroppedImage.setImageResource(testImage)
         selectedImage = BitmapFactory.decodeResource(resources, testImage)
-
-
     }
 
     private fun startTextRecognition() {
@@ -84,73 +90,24 @@ class receiptScanner : AppCompatActivity() {
         recognize.process(inputImage)
                 .addOnSuccessListener {
                     tvResult.text = it.text
-                    Log.d("TAG", "Successful recognition")
+                    Log.d(TAG, "Successful recognition")
                 }
                 .addOnFailureListener {
-                    Log.d("TAG", "Unsuccessful recognition", it)
+                    Log.d(TAG, "Unsuccessful recognition", it)
                 }
     }
 
-    private fun openDialog() {
-        val openDialog = AlertDialog.Builder(this@receiptScanner)
-        openDialog.setTitle("Choose image source")
-        openDialog.setPositiveButton("Camera") {
-            dialog, _->
-            openCamera()
-            dialog.dismiss()
-        }
-        openDialog.setNeutralButton("Gallery") {
-            dialog, _->
-            openGallery()
-        }
-        openDialog.setNegativeButton("Cancel") {
-            dialog, _->
-            dialog.dismiss()
-        }
-        openDialog.create()
-        openDialog.show()
 
     }
 
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(Intent.createChooser(intent, "Select Image"), 2)
-    }
 
-    private fun openCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        var file = File(Environment.getExternalStorageDirectory(), "file" + System.currentTimeMillis().toString() + ".jpg")
-        var uri = Uri.fromFile(file)
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-        intent.putExtra("return-data", true)
-        startActivityForResult(intent, 0)
 
-    }
 
-    private fun cropImages(){
-        UCrop.of(uri, destUri)
-            .withAspectRatio(16F, 9F)
-            .withMaxResultSize(300,180)
-            .start(this@receiptScanner);
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 0 && resultCode == RESULT_OK) {
-            cropImages()
-            if (data != null) {
 
-                    resultUri = UCrop.getOutput(data!!)!!
-            }
-        } else if (requestCode == 2) {
-            uri = data?.data!!
-            cropImages()
-        } else if (requestCode == 1) {
-            if (data != null) {
-                val bundle = data.extras
-                val bitmap = bundle!!.getParcelable<Bitmap>("data")
-                ivCroppedImage.setImageBitmap(bitmap)
-            }
-        }
-    }
-}
+
+
+
+
+
+
