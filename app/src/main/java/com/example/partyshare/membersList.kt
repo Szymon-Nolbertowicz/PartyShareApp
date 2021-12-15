9f4fad947eace856c9d353cff71599d9184584a7
Szymon-Nolbertowicz <s.nolbertowicz@gmail.com>
2021-12-15 03:24:17 +0100

    expenses list, permission check etc

---
+++ app/src/main/java/com/example/partyshare/membersList.kt
@@ -0,0 +1,11 @@
+package com.example.partyshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class membersList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_members_list)
+    }
+}
