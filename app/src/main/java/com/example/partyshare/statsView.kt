1b7cd42d2c8b4eeacaf34b43e37739a03ad65038
Szymon-Nolbertowicz <s.nolbertowicz@gmail.com>
2022-01-04 03:42:26 +0100

    Last commit

---
+++ app/src/main/java/com/example/partyshare/statsView.kt
@@ -0,0 +1,11 @@
+package com.example.partyshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class statsView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stats_view)
+    }
+}
